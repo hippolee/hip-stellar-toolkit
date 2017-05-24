432d4d41819508ea98cb5f856c62488c82a4df62
litfb <litfb@yonyou.com>
2017-05-24 11:42:52 +0800

    log4j2配置，spring配置，pom配置

---
+++ toolkit/src/main/java/org/hippo/toolkit/controller/LoginController.java
@@ -0,0 +1,13 @@
+package org.hippo.toolkit.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * LoginController
+ * Created by litengfei on 2017/5/22.
+ */
+@Controller
+public class LoginController {
+
+
+}

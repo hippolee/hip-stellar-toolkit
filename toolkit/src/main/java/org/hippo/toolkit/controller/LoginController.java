50ebe74b4cc477cd6ef553dc32ff630daf928d51
litfb <litfb@yonyou.com>
2017-05-25 13:38:44 +0800

    配置

--- toolkit/src/main/java/org/hippo/toolkit/controller/LoginController.java
+++ toolkit/src/main/java/org/hippo/toolkit/controller/LoginController.java
@@ -1,13 +1,28 @@
 package org.hippo.toolkit.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * LoginController
  * Created by litengfei on 2017/5/22.
  */
 @Controller
+@RequestMapping()
 public class LoginController {
 
+    /** logger */
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping("/login")
+    public String login() {
+        logger.debug("aaa");
+        logger.info("bbb", new RuntimeException());
+        logger.warn("ccc{}ccc{}ccc", "A", "B");
+        logger.error("ddd");
+        return "login";
+    }
 
 }
